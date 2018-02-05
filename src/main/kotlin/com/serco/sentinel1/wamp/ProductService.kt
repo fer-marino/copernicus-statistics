e6cbf055292ccafceb69cdd341257b0c6f40380a
Fernando Marino <fernando.marino@serco.com>
2018-02-05 09:10:34 +0100

    zip output extraction

--- src/main/kotlin/com/serco/sentinel1/wamp/ProductService.kt
+++ src/main/kotlin/com/serco/sentinel1/wamp/ProductService.kt
@@ -7,38 +7,40 @@ import com.serco.sentinel1.wamp.model.Product
 import org.apache.http.entity.ContentType
 import org.apache.http.nio.entity.NStringEntity
 import org.elasticsearch.ElasticsearchException
-import org.elasticsearch.action.ActionListener
 import org.elasticsearch.action.DocWriteResponse
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest
 import org.elasticsearch.action.bulk.BulkRequest
 import org.elasticsearch.action.index.IndexRequest
-import org.elasticsearch.action.search.ClearScrollRequest
 import org.elasticsearch.action.search.SearchRequest
 import org.elasticsearch.action.search.SearchScrollRequest
 import org.elasticsearch.action.update.UpdateRequest
 import org.elasticsearch.client.RestHighLevelClient
 import org.elasticsearch.common.unit.TimeValue
 import org.elasticsearch.common.xcontent.XContentType
+import org.elasticsearch.index.query.QueryBuilders
 import org.elasticsearch.index.query.QueryBuilders.matchAllQuery
-import org.elasticsearch.rest.RestStatus
 import org.elasticsearch.search.Scroll
 import org.elasticsearch.search.builder.SearchSourceBuilder
+import org.elasticsearch.search.sort.FieldSortBuilder
+import org.elasticsearch.search.sort.SortOrder
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody
+import java.io.BufferedOutputStream
 import java.text.SimpleDateFormat
 import java.time.Duration
 import java.time.temporal.ChronoUnit
-import java.time.temporal.TemporalUnit
 import java.util.*
-import java.util.concurrent.TimeUnit
+import java.util.zip.ZipEntry
+import java.util.zip.ZipOutputStream
 
 
 @RestController
 class ProductService {
     @Autowired lateinit var esClient: RestHighLevelClient
+    @Autowired lateinit var config: WampConfig
 
     @RequestMapping("/api/rebuildProduct")
     fun rebuildIndex(@RequestParam(value = "from", required = true) from: String,
@@ -200,6 +202,85 @@ class ProductService {
 
         return ResponseEntity.ok("Import started")
     }
+
+    @RequestMapping(value = "/api/export", method = [RequestMethod.GET], produces = ["application/zip"])
+    @ResponseStatus(value = HttpStatus.OK)
+    fun export(@RequestParam(value = "from", required = true) start: String,
+               @RequestParam(value = "to", required = true) stop: String): ResponseEntity<StreamingResponseBody> {
+
+        val t = StreamingResponseBody {
+            val writer = ZipOutputStream(BufferedOutputStream(it))
+            writer.putNextEntry(ZipEntry("exrtact.csv"))
+
+            val scroll =  Scroll(TimeValue.timeValueHours(1))
+            val sortPublication= FieldSortBuilder("publishedHub").order(SortOrder.ASC)
+            val sortProdType= FieldSortBuilder("prodType").order(SortOrder.ASC)
+            val searchSourceBuilder = SearchSourceBuilder()
+                    .query(QueryBuilders.rangeQuery("publishedHub").from(start).to(stop))
+                    .size(100)
+                    .timeout(TimeValue.timeValueSeconds(10))
+                    .sort(sortPublication).sort(sortProdType)
+            val searchRequest = SearchRequest(config.indexName).source(searchSourceBuilder).scroll(scroll)
+
+            var searchResponse = esClient.search(searchRequest)
+            var scrollId = searchResponse.scrollId
+            var hits = searchResponse.hits.hits
+
+            var scrollRequest: SearchScrollRequest
+
+            val om = ObjectMapper()
+
+            writer.write("Name, start, stop, mission, dtId, orbit, prodType, timeliness, publishedHub\n".toByteArray())
+            while (hits != null && hits.isNotEmpty()) {
+                hits.asList().parallelStream().forEach {
+//                hits.forEach {
+                    try {
+                        val res = om.readValue(it.sourceAsString, Map::class.java)
+                        val attributes = res["attributes"] as Map<String?, String?>
+
+                        val p = Product(res["name"] as String,
+                                res["start"] as String,
+                                res["stop"] as String,
+                                res["mission"] as String,
+                                res["dtId"] as String,
+                                res["orbit"] as Int,
+                                res["prodType"] as String,
+                                res["timeliness"] as? String,
+                                res["crc"] as String,
+                                res["footprint"].toString(),
+                                res["publishedHub"] as String,
+                                attributes)
+
+                        p.timeliness = if(p.timeliness == null) "" else p.timeliness
+
+                        synchronized(writer, {
+                            writer.write("${p.name}, ${p.start}, ${p.stop}, ${p.mission}, ${p.dtId}, ${p.orbit}, ${p.prodType}, ${p.timeliness}, ${p.publishedHub}\n".toByteArray())
+                        })
+                    } catch (e: Exception) {
+                        println("Error during product retrieval of ${it.id}. Skipping. Detailed message: ${e.message}")
+                    } catch (e: NullPointerException) {
+                        println("NPE during product retrieval of ${it.id}. Line ${e.stackTrace[0]}, ${e.stackTrace[1]}")
+                    }
+                }
+
+                scrollRequest = SearchScrollRequest(scrollId).scroll(scroll)
+                searchResponse = esClient.searchScroll(scrollRequest)
+
+                scrollId = searchResponse.scrollId
+                hits = searchResponse.hits.hits
+            }
+
+            writer.closeEntry()
+            writer.close()
+
+
+        }
+        return ResponseEntity.accepted()
+                .header("content-disposition", "attachment; filename=\"export.zip\"")
+                .body(t);
+
+    }
+
 
 }
 
--- src/main/kotlin/com/serco/sentinel1/wamp/config/ElasticsearchConfigurator.kt
+++ src/main/kotlin/com/serco/sentinel1/wamp/config/ElasticsearchConfigurator.kt
@@ -6,35 +6,32 @@ import org.apache.http.auth.UsernamePasswordCredentials
 import org.apache.http.impl.client.BasicCredentialsProvider
 import org.elasticsearch.client.RestClient
 import org.elasticsearch.client.RestHighLevelClient
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 
 @Configuration
 class ElasticsearchConfigurator {
-    @Autowired private lateinit var config: ElasticsearchConfig
+    @Value("\${elasticSearch.url}") private lateinit var url: String
+    @Value("\${elasticSearch.port}") private lateinit var port: String
+    @Value("\${elasticSearch.scheme}") private lateinit var scheme: String
+    @Value("\${elasticSearch.user}") private lateinit var user: String
+    @Value("\${elasticSearch.password}") private lateinit var password: String
+    @Value("\${elasticSearch.prefix}") private lateinit var prefix: String
 
     @Bean
     fun getElasticsearchClient(): RestHighLevelClient {
         val credentialsProvider = BasicCredentialsProvider().apply {
-            setCredentials(AuthScope.ANY, UsernamePasswordCredentials(config.user, config.password))
+            setCredentials(AuthScope.ANY, UsernamePasswordCredentials(user, password))
         }
 
-        val builder = RestClient.builder(HttpHost(config.url, config.port, config.scheme))
+        val builder = RestClient.builder(HttpHost(url, port.toInt(), scheme))
                 .setHttpClientConfigCallback { it.setDefaultCredentialsProvider(credentialsProvider) }
+        if(!prefix.trim().isEmpty() && prefix.trim() != "/")
+            builder.setPathPrefix(prefix)
 
         return RestHighLevelClient(builder)
     }
 
 }
-
-@ConfigurationProperties(prefix = "elasticSearch")
-data class ElasticsearchConfig (
-        var url: String = "localhost",
-        var port: Int = 9200,
-        var scheme: String = "http",
-        var user: String = "elastic",
-        var password: String = "password"
-)
